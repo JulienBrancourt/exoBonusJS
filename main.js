9d7b03796242e629ef1be7e07c063e83b5b8cf5a
Julien Brancourt <brancourt.julien@hotmail.fr>
2024-08-27 09:05:37 +0200

    ajout local storage

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { Vehicule } from "./classe/vehicule.js";
 import { Ticket } from "./classe/ticket.js";
 
-let parking = []
+// let parking = []
 
 const kangoo = new Vehicule("Renault", "Kangoo", "240000", "2003");
 
@@ -16,35 +16,44 @@ let info = document.querySelector(".info");
 info.innerHTML = "Bonjour !"
 
 create.addEventListener("click", () => {
-    let monTicket = new Ticket(immatriculation.value, new Date())
-    parking.push(monTicket);
-    console.log(parking);
-    document.querySelector(".immatriculation").value = "";
-    info.innerHTML = `Veuillez prendre votre ticket pour le véhicule ${monTicket.immatriculation}`
-    info.classList.add("good")
-    reset("good");
-})
+	let monTicket = new Ticket(immatriculation.value, new Date());
+	let parking = JSON.parse(localStorage.getItem("parkingJson")) || [];
+	parking.push(monTicket);
+	console.log(parking);
+	document.querySelector(".immatriculation").value = "";
+	info.innerHTML = `Veuillez prendre votre ticket pour le véhicule ${monTicket.immatriculation}`;
+	info.classList.add("good");
+	reset("good");
+
+	let parkingJson = JSON.stringify(parking);
+	localStorage.setItem("parkingJson", parkingJson);
+});
 
 pay.addEventListener("click", () => {
 	let monImmatriculation = immatriculation.value;
 	console.log(monImmatriculation);
-	let vehiculeTrouve = parking.find(
+
+	let parkingJson = localStorage.getItem("parkingJson")
+	let parkingParse = JSON.parse(parkingJson) || [];
+
+	let vehiculeTrouve = parkingParse.find(
 		(v) => v.immatriculation === monImmatriculation
-    );
-    let indexVehicule = parking.findIndex(
+	);
+    let indexVehicule = parkingParse.findIndex(
 			(v) => v.immatriculation === monImmatriculation
 		);
 
 	if (vehiculeTrouve) {
 		let dateActuelle = new Date();
-		let duree = (dateActuelle - vehiculeTrouve.date) / 1000 / 60; // Convertir en minutes
+		let duree = (dateActuelle - vehiculeTrouve.date) / 1000 / 60;
 		console.log("Durée en minutes : " + duree);
 
 		let prix = tarif(duree).toFixed(2);
 		info.innerHTML = `Le prix à payer pour le véhicule ${vehiculeTrouve.immatriculation} est de ${prix} €`;
 		info.classList.add("toPay");
         reset("toPay");
-        parking.splice(indexVehicule,1)
+		parkingParse.splice(indexVehicule, 1)
+		localStorage.setItem("parkingJson", JSON.stringify(parkingParse))
 	} else {
 		info.innerHTML = `Le véhicule ${monImmatriculation} n'existe pas !`;
 		info.classList.add("bad");
